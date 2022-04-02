113b2555838fe1401d1483a057a4c5d33a1a4b68
Mauricio Torres <otarksk@gmail.com>
2022-04-02 17:28:54 -0300

    VolantesServiceImplement realizada

--- src/main/java/com/springBajo8/springBajo8/service/impl/VolantesServiceImplement.java
+++ src/main/java/com/springBajo8/springBajo8/service/impl/VolantesServiceImplement.java
@@ -1,4 +1,30 @@
 package com.springBajo8.springBajo8.service.impl;
 
-public class VolantesServiceImplement {
+import com.springBajo8.springBajo8.domain.Productos;
+import com.springBajo8.springBajo8.domain.Volantes;
+import com.springBajo8.springBajo8.repository.VolantesRepo;
+import com.springBajo8.springBajo8.service.VolantesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class VolantesServiceImplement implements VolantesService {
+
+    @Autowired
+    @Qualifier("volantes")
+    VolantesRepo volantesRepo;
+
+    @Override
+    public Mono<Volantes> save(Volantes volantes) {
+        return this.volantesRepo.save(volantes);
+    }
+
+    @Override
+    public Flux<Volantes> findAll() {
+        return this.volantesRepo.findAll();
+    }
+
 }
